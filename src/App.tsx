07e6c20355f088df4d2977d6bd90235af96b63ad
saw-code <saw.coding@gmail.com>
2023-01-09 03:54:42 +0300

    10_lesson-finish

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import {Header} from './Header'
 import {Technologies} from "./Technologies";
+import {Footer} from "./Footer";
+
+
 
 function App() {
   return (
     <div>
       <Header/>
       <Technologies/>
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Footer() {
+  return (
+    <div>
+      it-kamasutra.com
+    </div>
+  )
+}
