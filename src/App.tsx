773e93a0c7ff3e5a8eb2d99fcb38bae2eadb906b
saw-code <saw.coding@gmail.com>
2023-01-15 13:12:13 +0300

    22_lesson

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         <Header/>
         <Navbar/>
         <div className='app-wrapper-content'>
-          <Route path="/dialogs" component={Dialogs}/>
+          <Route exact path="/dialogs" component={Dialogs}/>
           <Route path="/profile" component={Profile}/>
           <Route path="/news" component={News}/>
           <Route path="/music" component={Music}/>
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 export function Dialogs() {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
         <div className={s.dialog + " " + s.active}>
-          DimOk
+          <NavLink to="/dialogs/1">DimOk</NavLink>
         </div>
         <div className={s.dialog}>
-          Masha
+          <NavLink to="/dialogs/2">Masha</NavLink>
         </div>
         <div className={s.dialog}>
-          Svets
+          <NavLink to="/dialogs/3">Sveta</NavLink>
         </div>
         <div className={s.dialog}>
-          Sasha
+          <NavLink to="/dialogs/4">Sasha</NavLink>
         </div>
         <div className={s.dialog}>
-          Viktor
+          <NavLink to="/dialogs/5">Viktor</NavLink>
         </div>
       </div>
       <div className={s.messages}>
